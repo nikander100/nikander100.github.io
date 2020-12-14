b5cb79ed4e642e26c4cd38bd202e502ab9637e14
mierenmanz <you@example.com>
2020-12-14 22:13:37 +0100

    fixed bug

--- myscripts.js
+++ myscripts.js
@@ -1,11 +1,10 @@
-const card = document.querySelector(".linkcard");
-const mainLink = document.querySelector(".main-link");
-
-function handleClick(event) {
-	const noTextSelected = !window.getSelection().toString();
-	if (noTextSelected) {	
-		mainLink.click();
+function handleClick(div) {
+	switch (div) {
+		case "codam":
+			window.open("https://www.codam.nl/", "_blank")
+			break;
+		case "temp":
+			window.open("https://deno.land/x/deno_ffmpeg@1.1.1", "_blank")
+			break;
 	}
 }
-
-card.addEventListener("click", handleClick);
