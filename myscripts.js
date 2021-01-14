65b1f84e9178ed19c73cdcb0689123fc293e6c3e
nikander100 <nvan-der@student.codam.nl>
2021-01-14 18:19:05 +0100

    Update myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -1,8 +1,14 @@
 const card = document.querySelector(".linkcard");
 const mainLink = document.querySelector(".main-link");
+const clickableElements = Array.from(card.querySelectorAll(".clickable"));
+
+clickableElements.forEach((ele) =>
+	ele.addEventListener("click", (e) => e.stopPropagation())
+);
 
 function handleClick(event) {
 	const noTextSelected = !window.getSelection().toString();
+	
 	if (noTextSelected) {	
 		mainLink.click();
 	}
