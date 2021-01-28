6b5b8240d4137024289cb21bf8a6731b1be81836
nvan-der <nvan-der@student.codam.nl>
2021-01-28 01:29:35 +0100

    progress, js wroking, analytics implent, BAT implement

--- myscripts.js
+++ myscripts.js
@@ -1,17 +1,10 @@
-const card = document.querySelector(".linkcard");
-const mainLink = document.querySelector(".main-link");
-const clickableElements = Array.from(card.querySelectorAll(".clickable"));
-
-clickableElements.forEach((ele) =>
-	ele.addEventListener("click", (e) => e.stopPropagation())
-);
-
-function handleClick(event) {
-	const noTextSelected = !window.getSelection().toString();
-	
-	if (noTextSelected) {	
-		mainLink.click();
+function handleButtons(linkto) {
+	switch (linkto) {
+		case "codam":
+			window.open("https://www.codam.nl/", "_blank");
+			break;
+		case "github":
+			window.open("https://github.com/nikander100", "_blank");
+			break;
 	}
 }
-
-card.addEventListener("click", handleClick);
