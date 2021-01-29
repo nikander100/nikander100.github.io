64f02ee241099aff4dc448c992133c88e6eee71b
nvan-der <ndvdschee@gmail.com>
2021-01-29 14:03:55 +0100

    big update

--- myscripts.js
+++ myscripts.js
@@ -1,10 +1,29 @@
 function handleButtons(linkto) {
 	switch (linkto) {
-		case "codam":
-			window.open("https://www.codam.nl/", "_blank");
+		case "youtube":
+			window.open("https://youtube.com/nikander100", "_blank");
 			break;
 		case "github":
 			window.open("https://github.com/nikander100", "_blank");
+			break;
+		case "codam":
+			window.open("https://www.codam.nl/", "_blank");
+	}
+}
+
+function handleNav(linkto) {
+	switch (linkto) {
+		case "index":
+			window.open("index.html", "_self");
+			break;
+		case "socials":
+			window.open("socials.html", "_self");
+			break;
+		case "portfolio":
+			window.open("portfolio.html", "_self");
+			break;
+		case "contact":
+			window.open("contact.html", "_self");
 			break;
 	}
 }
