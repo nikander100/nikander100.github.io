d4966feac57648e7221e5f97e3af88023e9ca62e
nvan-der <ndvdschee@gmail.com>
2021-02-10 15:53:13 +0100

    update social buttons

--- myscripts.js
+++ myscripts.js
@@ -6,7 +6,20 @@ function handleButtons(linkto) {
 		case "github":
 			window.open("https://github.com/nikander100", "_blank");
 			break;
+		case "twitch":
+			window.open("https://www.twitch.tv/nikander100", "_blank");
+			break;
+		case "instagram":
+			window.open("https://www.instagram.com/nikander100/", "_blank");
+			break;
+		case "discord":
+			window.open("https://discord.gg/xekggu8phq", "_blank");
+			break;
+		case "reddit":
+			window.open("https://www.reddit.com/r/nikander100", "_blank");
+			break;
 		case "codam":
 			window.open("https://www.codam.nl/", "_blank");
+			break;
 	}
 }
